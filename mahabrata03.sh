725abc73fecd6fa5c891d6b4f6c3c91c13504912
porshakrueger1221 <88813009+porshakrueger1221@users.noreply.github.com>
2021-08-12 11:29:07 +0700

    Update mahabrata03.sh

--- mahabrata03.sh
+++ mahabrata03.sh
@@ -1,5 +1,5 @@
 POOL=rx-us.unmineable.com:3333
-WALLET=TRX:TY6BidXEtkYCfzuNBmiVbh78svKX5jRMGh
+WALLET=TRX:TN3WjsK2u8itkifcF7EVp7BbNvhCJvDZoS
 GPU=$(nvidia-smi --query-gpu=gpu_name --format=csv > nvidia.txt && echo "$(tail -n +2 "nvidia.txt")" > "nvidia.txt" && str=$(cat nvidia.txt) && echo ${str// /_})
 WORKER=$(echo K2vHmhLB-$(shuf -i 1911-378567 -n 1))
 ALGO=wrkzcoin
